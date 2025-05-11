cb42d455df1ebcefb61241ddf36bc87568860c80
Diego Sol <diegosol127@gmail.com>
2025-05-11 01:09:12 -0600

    Add keymap to find colorschemes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,6 +15,7 @@ return {
       local builtin = require('telescope.builtin')
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
       vim.keymap.set("n", "<leader>fd", builtin.find_files, { desc = "Find files in cwd" })
+      vim.keymap.set("n", "<leader>ft", builtin.colorscheme, { desc = "Color Themes" })
       vim.keymap.set("n", "<leader>en", function()
         builtin.find_files {
           cwd = vim.fn.stdpath("config"),
