822d9e8dbe88f54b49c345abd83abe3e586ffbd2
Diego Sol <diegosol127@gmail.com>
2025-05-11 00:03:56 -0600

    Add telescope plugin and create basic keymaps

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      require('telescope').setup {
+        defaults = {
+          winblend = 25
+        }
+      }
+      local builtin = require('telescope.builtin')
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+      vim.keymap.set("n", "<leader>fd", builtin.find_files, { desc = "Find files in cwd" })
+      vim.keymap.set("n", "<leader>en", function()
+        builtin.find_files {
+          cwd = vim.fn.stdpath("config"),
+          { desc = "Find files in .config/nvim/" }
+        }
+      end)
+    end,
+  }
+}
